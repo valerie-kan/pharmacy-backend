b4bca4b7f410543e99605ff8cf8264395016e18d
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-05-31 22:48:23 +0300

    fix notFound

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,4 +1,8 @@
-export const notFoundHandler = (req, res) => {
+export const notFoundHandler = (req, res, next) => {
+  if (res.headersSent) {
+    return next(); // нічого не робимо, якщо вже щось відправлено
+  }
+
   res.json(404).json({
     message: `${req.url} not found`,
   });
