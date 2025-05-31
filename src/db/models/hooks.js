433375ab971e1655bbe426ac4da39c60bd7b0150
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-05-31 22:58:23 +0300

    fix hooks

--- src/db/models/hooks.js
+++ src/db/models/hooks.js
@@ -1,7 +1,7 @@
 export const handleSaveError = (error, doc, next) => {
   const { name, code } = error;
   error.status = name === 'MongoServerError' && code === 11000 ? 409 : 400;
-  next();
+  next(error);
 };
 
 export const setUpdateSettings = function (next) {
