4f4dc557efc6b17e1e6b32c6f34cadcc87ab3c86
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-06-11 14:50:55 +0300

    fix pagination

--- src/services/products.js
+++ src/services/products.js
@@ -2,22 +2,31 @@ import ProductCollection from '../db/models/Product.js';
 
 import { calcPaginationData } from '../utils/calcPaginationData.js';
 
-export const getProducts = async ({ page, perPage, filter = {} }) => {
+export const getProducts = async ({ page = 1, perPage = 10, filter = {} }) => {
   const limit = perPage;
-  const skip = (page - 1) * perPage;
+  const skip = (page - 1) * limit;
 
-  const productQuery = ProductCollection.find();
+  // const productQuery = ProductCollection.find();
+  // if (filter.category) {
+  //   productQuery.where('category').equals(filter.category);
+  // }
+  // if (filter.name) {
+  //   productQuery.where('name', filter.name);
+  // }
+  const query = {};
   if (filter.category) {
-    productQuery.where('category').equals(filter.category);
+    query.category = filter.category;
   }
   if (filter.name) {
-    productQuery.where('name', filter.name);
+    query.name = filter.name;
   }
 
-  const items = await productQuery.skip(skip).limit(limit);
-  const total = await ProductCollection.find()
-    .merge(productQuery)
-    .countDocuments();
+  // const items = await productQuery.skip(skip).limit(limit);
+  const items = await ProductCollection.find(query).skip(skip).limit(limit);
+  // const total = await ProductCollection.find()
+  //   .merge(productQuery)
+  //   .countDocuments();
+  const total = await ProductCollection.countDocuments(query);
 
   const paginationData = calcPaginationData({ total, page, perPage });
 
